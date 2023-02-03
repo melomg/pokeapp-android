e3d91086f8f10849ac227b6da6c6171d8dbb6817
Melih Gultekin <mmelihgultekin@gmail.com>
2023-02-03 16:22:38 +0100

    Add application and activity hilt annotations

--- app/src/main/java/com/melih/android/pokeapp/MainActivity.kt
+++ app/src/main/java/com/melih/android/pokeapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.melih.android.pokeapp.core.designsystem.theme.PokeAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/melih/android/pokeapp/PokeApplication.kt
+++ app/src/main/java/com/melih/android/pokeapp/PokeApplication.kt
@@ -1,5 +1,7 @@
 package com.melih.android.pokeapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class PokeApplication : Application()
