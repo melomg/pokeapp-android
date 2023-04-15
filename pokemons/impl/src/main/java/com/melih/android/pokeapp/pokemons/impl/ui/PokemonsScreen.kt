36b826438006e2ca2852fd9d25d769f9f47b78e8
Melih Gultekin <mmelihgultekin@gmail.com>
2023-04-15 10:40:13 +0200

    Fix ui side implementation

--- pokemons/impl/src/main/java/com/melih/android/pokeapp/pokemons/impl/ui/PokemonsScreen.kt
+++ pokemons/impl/src/main/java/com/melih/android/pokeapp/pokemons/impl/ui/PokemonsScreen.kt
@@ -6,26 +6,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.melih.android.pokeapp.pokemons.impl.ui.PokemonsState.Error
-import com.melih.android.pokeapp.pokemons.impl.ui.PokemonsState.Initial
-import com.melih.android.pokeapp.pokemons.impl.ui.PokemonsState.Success
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
 internal fun PokemonsScreen(
     modifier: Modifier = Modifier,
     viewModel: PokemonsViewModel = hiltViewModel(),
 ) {
-    val state = viewModel.state.collectAsStateWithLifecycle().value
+    val pokemons = viewModel.paging.collectAsLazyPagingItems()
 
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier,
     ) {
-        when (state) {
-            is Initial -> Text(text = "Pokemons Initial")
-            is Error -> Text(text = "Pokemons Error")
-            is Success -> Text(text = "Pokemons Success")
+        when (pokemons.loadState.refresh) {
+            is LoadState.Error -> {
+                Text(text = "Pokemons Error")
+            }
+            is LoadState.Loading -> {
+                Text(text = "Pokemons Loading")
+            }
+            else -> {
+                Text(text = "Pokemons Initial")
+            }
         }
     }
 }
--- pokemons/impl/src/main/java/com/melih/android/pokeapp/pokemons/impl/ui/PokemonsViewModel.kt
+++ pokemons/impl/src/main/java/com/melih/android/pokeapp/pokemons/impl/ui/PokemonsViewModel.kt
@@ -2,12 +2,12 @@ package com.melih.android.pokeapp.pokemons.impl.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.melih.android.pokeapp.pokemons.api.data.PokemonsRepository
-import com.melih.android.pokeapp.pokemons.api.model.Pokemons
+import com.melih.android.pokeapp.pokemons.api.model.Pokemon
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,23 +15,7 @@ internal class PokemonsViewModel @Inject constructor(
     repository: PokemonsRepository,
 ) : ViewModel() {
 
-    val state: StateFlow<PokemonsState>
-        get() = _state
-
-    private val _state = MutableStateFlow<PokemonsState>(PokemonsState.Initial)
-
-    init {
-        viewModelScope.launch {
-            repository.getPokemons(0, 0) // todo implement pagination later
-                .toUIState()
-                .let { newState ->
-                    _state.value = newState
-                }
-        }
-    }
+    val paging: Flow<PagingData<Pokemon>> = repository
+        .getPokemons()
+        .cachedIn(viewModelScope)
 }
-
-private fun Result<Pokemons>.toUIState(): PokemonsState = fold(
-    onSuccess = { PokemonsState.Success },
-    onFailure = { PokemonsState.Error(it) },
-)
