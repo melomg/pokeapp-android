a400989408428c3ddc89a2722dffe3578fd8ba08
Melih Gultekin <mmelihgultekin@gmail.com>
2023-05-29 12:58:46 +0200

    Fix detekt issues

--- app/src/main/java/com/melih/android/pokeapp/MainActivity.kt
+++ app/src/main/java/com/melih/android/pokeapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.melih.android.pokeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.melih.android.pokeapp.core.designsystem.theme.PokeAppTheme
 import com.melih.android.pokeapp.core.navigation.Routers
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,13 +22,5 @@ class MainActivity : ComponentActivity() {
                 MainScreen(routers = routers)
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokeAppTheme {
-        MainScreen(routers = emptyMap())
     }
 }
