89b5b3a63fb9e10005bc699209dad4cd09b2158c
Melih Gultekin <mmelihgultekin@gmail.com>
2023-09-10 12:48:57 +0200

    Bump min sdk version to 27

--- build-logic/convention/src/main/kotlin/com/melih/apps/pokeapp/SdkVersions.kt
+++ build-logic/convention/src/main/kotlin/com/melih/apps/pokeapp/SdkVersions.kt
@@ -2,6 +2,6 @@ package com.melih.apps.pokeapp
 
 object SdkVersions {
     const val compileSdkVersion: Int = 34
-    const val minSdkVersion: Int = 26
+    const val minSdkVersion: Int = 27
     const val targetSdkVersion: Int = 34
 }
